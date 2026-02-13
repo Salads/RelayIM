320da25a5e9fb1d48f87b5329ce67106ede9feeb
Salads <dgentry.email@protonmail.com>
2026-02-13 00:39:06 -0800

    Tweak chat input size to 30

--- RelayClient/src/components/QChatInput.cpp
+++ RelayClient/src/components/QChatInput.cpp
@@ -6,12 +6,13 @@ QChatInput::QChatInput(QWidget *parent)
     : QWidget(parent)
 {
     ui.setupUi(this);
-    setFixedHeight(50);
+    setFixedHeight(30);
 
     setAttribute(Qt::WA_StyledBackground, true);
     setStyleSheet("QWidget { background-color: #f0f0f0; border: 1px solid #cccccc; border-radius: 5px; }");
 
     QHBoxLayout* mainLayout = new QHBoxLayout(this);
+    mainLayout->setContentsMargins(5, 2, 5, 2);
 
     QTextEdit* inputField = new QTextEdit(this);
     inputField->setPlaceholderText("Enter message...");
