1cfc943d8f92c9565afce35ad9e0f9e3764da47b
Salads <dgentry.email@protonmail.com>
2026-02-14 00:00:08 -0800

    Added packetype enum to DLL

---
+++ RelayIMNetworkShared/src/PacketType.h
@@ -0,0 +1,14 @@
+#pragma once
+
+
+enum PacketType
+{
+    PacketType_Connect = 0,
+    PacketType_Disconnect,
+    PacketType_HeartBeat,
+    PacketType_CreateChatRoom,
+    PacketType_JoinChatRoom,
+    PacketType_LeaveChatRoom,
+    PacketType_SendMessage,
+
+};
