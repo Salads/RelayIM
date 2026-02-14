84fa30e4bcc77af111a42f14ff9518c3abc45c27
Salads <dgentry.email@protonmail.com>
2026-02-13 23:59:51 -0800

    Added DLL skeleton for shared network code

---
+++ RelayIMNetworkShared/src/DLLTypes.h
@@ -0,0 +1,4 @@
+#pragma once
+
+#define EXPORTFUNC extern "C" __declspec(dllexport)
+#define EXPORTCLASS __declspec(dllexport)
---
+++ RelayIMNetworkShared/src/NetworkClient.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#include "DLLTypes.h"
+
+// TODO(Salads): NetworkClient which handles network packing and sending/receiving data for both Server and Client.
+class EXPORTCLASS NetworkClient
+{
+
+};
