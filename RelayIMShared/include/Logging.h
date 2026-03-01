a524348e3164d6aa0f35f8bf6f5dfe639d05c319
Salads <dgentry.email@protonmail.com>
2026-03-01 01:18:42 -0800

    Added some cool Logging functions that work with std::string
    Instead of using a va_list type stuff, which destroys type information, I use Args.
    
    Then, the simpler LogArgument function just returns the type. The specialized version converts a string to it's c_str(). We use this template function for each of the variadic arguments, so that if it's a std::string, we get it's c string version. WE pass these args to printf, and success, we can just use std::string in our log to stdout!

--- RelayIMShared/include/Logging.h
+++ RelayIMShared/include/Logging.h
@@ -1,8 +1,46 @@
 #pragma once
 
+#include <mutex>
+
 #include "NetworkTypes.h"
+
+#define LOGGING_SPACES_PER_DEPTH 2
 
 #define LOG_NETWORK_BYTESTREAM false
 
-void LogDepth(uint32_t depth, const char* fmt, ...);
-void LogDepthConditional(bool cond, uint32_t depth, const char* fmt, ...);
+extern std::mutex g_loggingMutex;
+
+template <typename T>
+T LogArgument(T value) noexcept
+{
+    return value;
+};
+
+template <typename T>
+T const* LogArgument(std::basic_string<T> const& value) noexcept
+{
+    return value.c_str();
+};
+
+template <typename ... Args>
+void LogDepth(uint32_t depth, const char* fmt, Args const & ... args) noexcept
+{
+    std::lock_guard lock(g_loggingMutex);
+
+    std::string str = fmt;
+
+    if (depth > 0)
+    {
+        str = std::string(depth * LOGGING_SPACES_PER_DEPTH, ' ') + str;
+    }
+
+    printf(str.c_str(), LogArgument(args) ...);
+};
+
+template <typename ... Args>
+void LogDepthConditional(bool cond, uint32_t depth, const char* fmt, Args const & ... args) noexcept
+{
+    if (!cond) { return; }
+
+    LogDepth(depth, fmt, args ...);
+}
--- RelayIMShared/src/Logging.cpp
+++ RelayIMShared/src/Logging.cpp
@@ -5,37 +5,4 @@
 #include <thread>
 #include <mutex>
 
-#define LOGGING_SPACES_PER_DEPTH 2
-
-static std::mutex g_loggingMutex;
-
-void LogDepth(uint32_t depth, const char* fmt, ...)
-{
-    std::lock_guard lock(g_loggingMutex);
-
-    va_list args;
-    va_start(args, fmt);
-
-    std::string str = fmt;
-
-    if (depth > 0)
-    {
-        str = std::string(depth * LOGGING_SPACES_PER_DEPTH, ' ') + str;
-    }
-
-    vprintf(str.c_str(), args);
-
-    va_end(args);
-}
-
-void LogDepthConditional(bool cond, uint32_t depth, const char* fmt, ...)
-{
-    if (!cond) { return; }
-
-    va_list args;
-    va_start(args, fmt);
-
-    LogDepth(depth, fmt, args);
-
-    va_end(args);
-}
+std::mutex g_loggingMutex;
